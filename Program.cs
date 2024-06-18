aa7cd7fd6e6da4ef0d7f5e034fd1eca8baa85353
Rafael Lopes <rafaellopesdonascimento2607@gmail.com>
2024-06-17 23:09:42 -0300

    Calculo de media

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioDois
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<double> list = new List<double>();
+            double value = double.Parse(Console.ReadLine());
+       
+            void ReturnValue(double valueParam)
+            {
+                if (valueParam >= 0)
+                {
+                    list.Add(valueParam);
+                } else if (list.Count <= 0 && valueParam < 0)
+                {
+                    Console.WriteLine("Impossible to calculate");
+                }
+            }
+
+            double Media(List<double> listParam) {
+                double total = 0;
+                if (listParam.Count > 0)
+                {
+                    
+                foreach (var item in listParam)
+                {
+                    total += item;
+                }
+                return total/listParam.Count;
+                } else
+                {
+                    Console.WriteLine("Impossible to calculate media");
+                    return total;
+                }
+            }
+
+            while (value > 0)
+            {
+                ReturnValue(value);
+                value = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(Media(list).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
